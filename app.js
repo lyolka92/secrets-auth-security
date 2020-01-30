a0d915d1ee122f323b138d5335598ee3dabf3c89
Olga Serova <lyolka92@gmail.com>
2020-01-30 11:03:26 +0300

    Google oauth is added

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -31,21 +33,48 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
- 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: ""
+  },
+  (accessToken, refreshToken, profile, done) => {
+       User.findOrCreate({googleId: profile.id,},
+        (err, user) => {
+         return done(err, user);
+       });
+  }
+));
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", {scope: ["https://www.googleapis.com/auth/plus.login"]})
+);
 
 app.get("/login", (req, res) => {
     res.render("login");
@@ -53,6 +82,11 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", {failureRedirect: "/login"}), (req, res) => {
+    res.redirect("/secrets");
 });
 
 app.get("/secrets", (req,res) => {
@@ -76,7 +110,7 @@ app.post("/register", (req, res) => {
         } else {
             passport.authenticate("local")(req, res, () => {
                 res.redirect("/secrets");
-            })
+            });
         }
     });
 });
